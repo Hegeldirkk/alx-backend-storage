ddff078ec7d73d2bffc9e78d3716ac15db49acd7
Hegeldirkk <hegeldirkk@gmail.com>
2023-01-17 23:03:55 +0000

    script git writting - ok now 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -7,8 +7,4 @@ import pymongo
 
 def list_all(mongo_collection):
     """return empty if not doc"""
-    school = mongo_collection.find()
-    if not mongo_collection:
-        return []
-    for doc in school:
-        return [doc]
+    return [ doc for doc in mongo_collection.find() ]
